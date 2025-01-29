07759a7c181b0b109ad883481cac0c7e95530b4d
Kayla E <kengelstad16@gmail.com>
2025-01-29 09:48:52 -0600

    veiwing options and queries

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import { QueryResult } from 'pg';
 import { pool, connectToDb } from './connection.js';
+import Cli from './cli.js'
 
 await connectToDb();
 
@@ -7,3 +8,14 @@ const sql = `SELECT * FROM department`;
 const result = await pool.query(sql);
 
 console.table(result.rows);
+
+const cli = new Cli();
+cli.performActions();
+
+
+// pool.end() not finishing, and neither does the program w/o Ctrl+C
+/*
+console.log('calling end')
+await pool.end()
+console.log('pool has drained')
+*/
